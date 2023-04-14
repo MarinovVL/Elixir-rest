eb65ce0419348ae40c31c55611964486976c53e2
MarinovVL <123395452+MarinovVL@users.noreply.github.com>
2023-04-14 14:44:00 +0300

    add sale functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, abort, make_response
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
@@ -159,6 +159,113 @@ class BarcodeResource(Resource):
         return {"message": "Barcode added successfully"}, 201
 
 
+def create_or_get_sale_order(sale_order_id):
+    if sale_order_id:
+        sale_order = SaleOrder.query.get(sale_order_id)
+        if not sale_order:
+            sale_order = SaleOrder(id=sale_order_id)
+            db.session.add(sale_order)
+            db.session.commit()
+    else:
+        sale_order = SaleOrder()
+        db.session.add(sale_order)
+        db.session.commit()
+        sale_order_id = sale_order.id
+    return sale_order_id
+
+
+def get_medicine_id(barcode):
+    medicine_barcode = MedicineBarcode.query.filter_by(barcode_1=barcode).first() or \
+                       MedicineBarcode.query.filter_by(barcode_2=barcode).first()
+
+    if medicine_barcode:
+        medicine_id = medicine_barcode.medicine_id
+    else:
+        # default medicine_id for "Medicine without barcode"
+        medicine_id = 18
+    return medicine_id
+
+
+class SaleResource(Resource):
+    def post(self):
+        barcode = request.json.get('barcode')
+        quantity = float(request.json.get('quantity', 1))
+        sale_order_id = request.json.get('sale_order_id')
+
+        sale_order_id = create_or_get_sale_order(sale_order_id)
+        medicine_id = get_medicine_id(barcode)
+
+        inventory = Inventory.query.filter_by(medicine_id=medicine_id).first()
+
+        if inventory and inventory.quantity >= quantity:
+            inventory.quantity -= quantity
+            db.session.add(inventory)
+            sale_price = inventory.price
+        elif inventory:
+            inventory.quantity = 0
+            db.session.add(inventory)
+            sale_price = inventory.price
+        else:
+            # Set a default price when the inventory is not found
+            sale_price = None
+
+        sale = Sale(medicine_id=medicine_id,
+                    quantity=quantity,
+                    price=sale_price,
+                    sale_order_id=sale_order_id)
+
+        db.session.add(sale)
+        db.session.commit()
+
+        medicine = MedicineDetail.query.get(medicine_id)
+        result = {
+            'medicine_name': medicine.medicine_name_bg or medicine.medicine_name,
+            'quantity': quantity,
+            'price': sale_price
+        }
+
+        if medicine.opiate:
+            result['opiate'] = True
+
+        return result, 201
+
+    def put(self, sale_id):
+        sale = Sale.query.get(sale_id)
+
+        if not sale:
+            abort(404)
+
+        quantity = request.json.get('quantity')
+        price = request.json.get('price')
+        sale_order_id = request.json.get('sale_order_id')
+
+        if quantity is not None:
+            sale.quantity = quantity
+
+        if price is not None:
+            sale.price = price
+
+        if sale_order_id is not None:
+            sale.sale_order_id = sale_order_id
+
+        db.session.add(sale)
+        db.session.commit()
+
+        return make_response("", 204)
+
+    def delete(self, sale_id):
+        sale = Sale.query.get(sale_id)
+
+        if not sale:
+            abort(404)
+
+        db.session.delete(sale)
+        db.session.commit()
+
+        return make_response("", 204)
+
+
+api.add_resource(SaleResource, '/sale', '/sale/<int:sale_id>')
 api.add_resource(PurchaseResource, '/purchase')
 api.add_resource(BarcodeResource, '/add_barcode')
 api.add_resource(LandingPage, '/')
@@ -216,16 +323,30 @@ class Purchase(db.Model):
     purchase_order = db.Column(db.String(255))
 
 
+class SaleOrder(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    timestamp = db.Column(db.DateTime, nullable=False, server_default=func.now())
+
+    sales = db.relationship('Sale', backref='sale_order', lazy=True)
+
+
 class Sale(db.Model):
     __tablename__ = 'sale'
     sale_id = db.Column(db.Integer, primary_key=True)
     medicine_id = db.Column(db.Integer, db.ForeignKey('medicine_detail.medicine_id'))
-    quantity = db.Column(db.Integer)
+    quantity = db.Column(db.Float)
     price = db.Column(db.Float)
     timestamp = db.Column(db.DateTime, server_default=func.now())
     batch_number = db.Column(db.String(255))
     verified = db.Column(db.Boolean)
     reported = db.Column(db.Boolean)
+    sale_order_id = db.Column(db.Integer, db.ForeignKey('sale_order.id'), nullable=False)
+
+    def __init__(self, medicine_id, quantity, price, sale_order_id):
+        self.medicine_id = medicine_id
+        self.quantity = quantity
+        self.price = price
+        self.sale_order_id = sale_order_id
 
 
 if __name__ == '__main__':
